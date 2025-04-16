c810444a557fe4e1962db84d795f43dff329eb0d
Givaldogs <givaldowanda@gmail.com>
2025-04-16 08:57:45 -0300

    aula 546, Iniciando a parte interna do Post (varios detalhes interessantes

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import index, post, page
+from blog.views import index, post, page, created_by, category
 
 
 app_name = 'blog'
@@ -12,7 +12,7 @@ urlpatterns = [
      path('post/<slug:slug>/', post, name='post'),
      path('page/', page, name='page'),
      path('page/<slug:slug>/', page, name='page'),
-    # path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    #path('category/<slug:slug>/', category, name='category'),
+     path('created_by/<int:author_pk>/', created_by, name='created_by'),
+     path('category/<slug:slug>/', category, name='category'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,40 @@ def index(request):
     )
 
 
+def created_by(request, author_pk):
+     posts = Post.objects.get_published()\
+         .filter(created_by__pk=author_pk)
+ 
+     paginator = Paginator(posts, PER_PAGE)
+     page_number = request.GET.get("page")
+     page_obj = paginator.get_page(page_number)
+ 
+     return render(
+         request,
+         'blog/pages/index.html',
+         {
+             'page_obj': page_obj,
+         }
+     )
+
+
+def category(request, slug):
+     posts = Post.objects.get_published()\
+         .filter(category__slug=slug)
+ 
+     paginator = Paginator(posts, PER_PAGE)
+     page_number = request.GET.get("page")
+     page_obj = paginator.get_page(page_number)
+ 
+     return render(
+         request,
+         'blog/pages/index.html',
+         {
+             'page_obj': page_obj,
+         }
+     )
+
+
 def page(request):
     # paginator = Paginator(posts, 9)
     # page_number = request.GET.get("page")
@@ -39,12 +73,16 @@ def post(request, slug):
     # paginator = Paginator(posts, 9)
     # page_number = request.GET.get("page")
     # page_obj = paginator.get_page(page_number)
-
+    post = (
+        Post.objects.get_published()
+        .filter(slug=slug)
+        .first()
+    )
     return render(
         request,
         'blog/pages/post.html',
         {
-           #  'page_obj': page_obj,
+             'post': post,
         }
     )
 
@@ -65,7 +103,7 @@ def post_list(request):
     )
 
 # Página estática
-def page(request):
+def page(request, slug):
     # Se quiser, podemos adaptar isso pra buscar uma Page específica também.
     return render(
         request,
