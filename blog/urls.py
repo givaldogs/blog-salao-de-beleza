ee60a8373db1f1243164f21cf32a8b88abca136a
Givaldogs <givaldowanda@gmail.com>
2025-04-27 18:56:41 -0300

    Aula 552- Comtemplando o nosso model Page para paginas livres com HTML

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import index, post, page, created_by, category, tag
+from blog.views import index, post, page, created_by, category, tag, search
 
 
 app_name = 'blog'
@@ -8,12 +8,13 @@ app_name = 'blog'
 
 urlpatterns = [
      path('', index, name='index'),
-     path('post/', post, name='post'),
+     #path('post/', post, name='post'),  # nao tem no do professor
      path('post/<slug:slug>/', post, name='post'),
-     path('page/', page, name='page'),
+     #path('page/', page, name='page'),  # nao tem no do professor
      path('page/<slug:slug>/', page, name='page'),
      path('created_by/<int:author_pk>/', created_by, name='created_by'),
      path('category/<slug:slug>/', category, name='category'),
      path('tag/<slug:slug>/', tag, name='tag'),
+     path('search/', search, name='search'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Page, Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -72,24 +73,48 @@ def tag(request, slug):
      )
 
 
-def page(request):
-    # paginator = Paginator(posts, 9)
-    # page_number = request.GET.get("page")
-    # page_obj = paginator.get_page(page_number)
+def search(request,):
+     search_value = request.GET.get('search', '').strip()
+ 
+     posts = (
+         Post.objects.get_published()
+         .filter(
+             Q(title__icontains=search_value) |
+             Q(excerpt__icontains=search_value) |
+             Q(content__icontains=search_value)
+         )[:PER_PAGE]
+     )
+ 
+     return render(
+         request,
+         'blog/pages/index.html',
+         {
+             'page_obj': posts,
+             'search_value': search_value,
+         }
+     )
+ 
+
+def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
 
     return render(
         request,
         'blog/pages/page.html',
         {
-           #  'page_obj': page_obj,
+            'page': page,
         }
     )
 
+ 
 # Detalhe do post
 def post(request, slug):
-    # paginator = Paginator(posts, 9)
-    # page_number = request.GET.get("page")
-    # page_obj = paginator.get_page(page_number)
+    
     post = (
         Post.objects.get_published()
         .filter(slug=slug)
